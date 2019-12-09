8754753e14e87e3829c2f3eade1482c0108231e1
mattshin64 <mattshin64@gmail.com>
2019-12-09 15:24:05 -0800

    changes server port number

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
 /* eslint-disable no-console */
 const app = require('./index.js');
 
-app.listen(3001, () => console.log('Listening in on port: 3001'));
+app.listen(3002, () => console.log('Listening in on port: 3002'));
