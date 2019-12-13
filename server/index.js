78013db028c3f4f9630d06dd4c92e96426bef26b
mattshin64 <mattshin64@gmail.com>
2019-12-13 15:29:10 -0800

    invokes compression

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ const Controller = require('./controller.js');
 
 app.use(express.static(path.join(__dirname, '../public')));
 app.use(cors());
+app.use(compression());
 
 app.get('/api/estimates/pricing/:houseId', (req, res) => {
   Controller.getgraph(req, res);
